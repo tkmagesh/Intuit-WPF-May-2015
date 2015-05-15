da767d8cc145c9020aa0c2c84b5cf0fda9c3902c
tkmagesh <tkmagesh77@gmail.com>
2015-05-15 11:54:43 +0530

    Collections Demo - Sed

---
+++ Day-01/01-Language/LanguageIntro/LanguageIntro/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee(100) {Name = "Magesh", Salary = 20000};
+            employee.Name = "Magesh";
+            Console.WriteLine("Employee Id = {0}, Name = {1}", employee.Id, employee.Name);
+            employee.Display();
+            ICricketer cricketer = (ICricketer) employee;
+            
+            Console.ReadLine();
+        }
+    }
+
+    abstract class EmployeeBase
+    {
+        public int Id { get; set; }
+
+        public virtual void Display(){}
+    }
+
+    class Employee : EmployeeBase, ICricketer
+    {
+        public string Name { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public Employee(int id)
+        {
+            Id = id;
+        }
+
+        
+
+        public override void Display()
+        {
+            Console.WriteLine("Id = {0}, Name = {1}, Salary = {2}", Id, Name, Salary);
+        }
+
+        public string Ball { get; set; }
+
+        void ICricketer.Bowl()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Bat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Field()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface ICricketer
+    {
+        string Ball { get; set; }
+        void Bowl();
+        void Bat();
+        void Field();
+    }
+}
---
+++ Day-01/01-Language/ProductManagementApp/ProductManagementApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManagementApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var product = new Product(){Id = 1, Name = "Pen", Units = 10, Cost = 25, Category = 1};
+            Console.WriteLine(product);
+            Console.ReadLine();
+
+            //Create an instance of ProductsCollection
+            //Add a few products to the collection
+            //Iterate through the collection and print them
+
+        }
+    }
+
+    class ProductsCollection
+    {
+         private ArrayList _list = new ArrayList();
+
+        //Add
+        //Removing
+        //Get a product by index
+        //Count
+    }
+
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Units { get; set; }
+        public decimal Cost { get; set; }
+        public int Category { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Id = {0}\t Name = {1}\t Units = {2}\t Cost = {3}\t Category = {4}", this.Id, Name,
+                Units, Cost, Category);
+        }
+    }
+}
