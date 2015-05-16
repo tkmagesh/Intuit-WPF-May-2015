ed298ba2ca959a1c678b6128bee8415997e1fa45
tkmagesh <tkmagesh77@gmail.com>
2015-05-16 16:29:56 +0530

    DataBinding and Converters

--- Day-02/WPF/MyFirstWPFApp/MyFirstWPFApp/MainWindow.xaml.cs
+++ Day-02/WPF/MyFirstWPFApp/MyFirstWPFApp/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +24,27 @@ namespace MyFirstWPFApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BtnOk_Click(object sender, RoutedEventArgs e)
+        {
+            var userName = TxtUserName.Text;
+            var password = PwdPassword.Password;
+            if (userName == "Magesh" && password == "password")
+            {
+                TextBlockStatus.Text = "Login Successful!";
+                TextBlockStatus.Foreground = new System.Windows.Media.SolidColorBrush(Colors.Green);
+            }
+            else
+            {
+                TextBlockStatus.Text = "Wrong creditials! Login Failure!!";
+                TextBlockStatus.Foreground = new System.Windows.Media.SolidColorBrush(Colors.Red);
+            }
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            TxtUserName.Text = PwdPassword.Password = string.Empty;
         }
     }
 }
